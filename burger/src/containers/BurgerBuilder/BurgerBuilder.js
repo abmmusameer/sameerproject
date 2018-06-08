1fda80bef6351230be9b2b7e7729ed1918b29792
sameer Phatak <sam.phatak8@gmail.com>
2018-06-08 17:34:04 +0530

    added

--- burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
 
-import Aux from '../../hoc/Aux';
+
 import Burger from '../../components/Burger/Burger';
+import BuildControls from  '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
     constructor(props){
         super(props);
         this.state = { 
             ingredients: {
-                salad: 1,
-                bacon: 1,
-                cheease: 2,
-                meat: 2
+                salad: 0,
+                bacon: 0,
+                cheease: 0,
+                meat: 0
             }
       }
     }
  
     render () {
         return (
-            <Aux>
+         
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Build Controls</div>
-            </Aux>
+                <BuildControls />
+           
         );
     }
 }
