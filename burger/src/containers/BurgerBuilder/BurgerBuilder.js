6a0fa202d3a30ff448828b25706ac163a579120e
sameer Phatak <sam.phatak8@gmail.com>
2018-06-08 18:12:35 +0530

    added

--- burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-
+import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from  '../../components/Burger/BuildControls/BuildControls';
 
@@ -19,10 +19,10 @@ class BurgerBuilder extends Component {
  
     render () {
         return (
-         
+            <Aux>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls />
-           
+            </Aux>
         );
     }
 }
