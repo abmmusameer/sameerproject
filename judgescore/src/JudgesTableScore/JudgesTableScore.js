1fd8db4845211b285ab59480cffe466f02b5fec7
sameer Phatak <sam.phatak8@gmail.com>
2018-06-28 17:45:54 +0530

    mes

--- judgescore/src/JudgesTableScore/JudgesTableScore.js
+++ judgescore/src/JudgesTableScore/JudgesTableScore.js
@@ -1,9 +1,28 @@
 import React, {Component} from 'react';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 
 import './JudgesTableScore.css';
 
 class Judgetablescore extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            selectedOption: '',
+            technique: 0,
+        }
+    }
+    
+    //select option
+    handleChange = (selectedOption) => {
+        this.setState({ selectedOption });        
+        if (selectedOption) {
+            document.querySelector('.selectedTeamname').textContent = selectedOption.label;            
+        }
+    }
     render(){
+        const { selectedOption } = this.state;
         return(
             <section className="judgesTablePanel">
               <div className="container">
@@ -11,29 +30,37 @@ class Judgetablescore extends Component{
                     <div>Lauren</div>
                 </figure> 
                 <div className="team_select">
-                    <select>
-                        <option value="">Team 1</option>
-                        <option value="">Team 1</option>
-                        <option value="">Team 1</option>
-                        <option value="">Team 1</option>
-                        <option value="">Team 1</option>
-                        <option value="">Team 1</option>
-                        <option value="">Team 1</option>
-                        <option value="">Team 1</option>
-                        <option value="">Team 1</option>
-                        <option value="">Team 1</option>
-                    </select>
-                    <p>Team 1</p>
+                    <Select
+                        name="form-field-name"
+                        value={selectedOption}
+                        onChange={this.handleChange}
+                        options={[
+                            { value: 'one', label: 'One' },
+                            { value: 'two', label: 'Two' },
+                            { value: 'three', label: 'Three' },
+                        ]}
+                    />
+                    <p className="selectedTeamname">Team 1</p>
                 </div>
 
                 <div className="categoryPanel">
                     <div className="row">
-                        <div className="col-8">
-                            Category 1
-                        </div>
-                        <div className="col-4">
-                            <input type="text"/>
-                        </div>
+                        <div className="col-8">Technical Presentation</div>
+                            <div className="col-4">
+                            <input type="text" 
+                            name="techPresentation" 
+                            id="techPresentation" 
+                            value={this.state.techPresentation} onChange={this.handlechange}/></div>
+                            <ul>
+                                <li>
+                                    <label>
+                                        Technique
+                                        <input type="text" name="technique" id="technique" placeholder="0"
+                                        
+                                        />
+                                    </label>
+                                </li>
+                            </ul>
                     </div>
                     <div className="row">
                         <div className="col-8">
