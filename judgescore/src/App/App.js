d94f4890ff353311cb4621139d6235afc151a568
abmmusameer <sam.phatak8@gmail.com>
2018-07-09 23:03:36 +0530

    msg

--- judgescore/src/App/App.js
+++ judgescore/src/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-// import Header from '../Header/Header';
+import Header from '../Header/Header';
 import Main from '../Main/main';
 
 import './App.css';
@@ -9,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App container">
-       {/* <Header/> */}
+       <Header/>
        <Main/>
       </div>
     );
